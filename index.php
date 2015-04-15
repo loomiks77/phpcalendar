38415ddb8526c67c47e58556196add66c1bbf827
Michael Lowicki <lowickimichael@gmail.com>
2015-04-15 21:01:03 +0100

    Basic upload
    This is the start point of second version of the calendar

---
+++ index.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Web Calendar</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div id="big_wrapper">
+		<header id="top_header">
+			<h1>Welcome to Matt's Website</h1>
+		</header>
+		
+		<table id="tableweek">
+			<tr>
+				<th>Time</th>
+				<th>Monday</th>
+				<th>Tuesday</th>
+				<th>Wednesday</th>
+				<th>Thursday</th>
+				<th>Friday</th>
+			</tr>
+
+			 <tr>
+   				 <td>9:00</td>
+   				 <td>ELEMENT</td>
+   				 <td>ELEMENT</td>
+ 			 </tr>
+
+			</tr>
+
+
+		</table>
+			
+			<aside id="side_news">
+				<h4>News</h4>
+				Website is currently under development
+			</aside>
+		</div>
+		<footer id="the_footer">
+			Copyright MattEtherton 2015
+		</footer>
+	</div>
+</body>
+</html> 
+
+
+<?php
+echo 'Hello World';
+?>
---
+++ old.php
@@ -0,0 +1,64 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Matt Etherton's Website</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div id="big_wrapper">
+		<header id="top_header">
+			<h1>Welcome to Matt's Website</h1>
+		</header>
+		
+		<nav id="top_menu">
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			<ul>
+		</nav>
+		<div id="new_div">
+			<section id="main_section">
+				<article>
+					<header>
+						<hgroup>
+							<h1>Title of article</h1>
+							<h2>Subtitle for article</h2>
+						</hgroup>
+					</header>
+					<p>This is an article!</p>
+					<footer>
+						<p>- Written by Matt</p>
+					</footer>
+				</article>
+				
+				<article>
+					<header>
+						<hgroup>
+							<h1>Title of article 2</h1>
+							<h2>Subtitle for article2</h2>
+						</hgroup>
+					</header>
+					<p>This is an article! 2</p>
+					<footer>
+						<p>- Written by Matt</p>
+					</footer>
+				</article>	
+			</section>
+			
+			<aside id="side_news">
+				<h4>News</h4>
+				Website is currently under development
+			</aside>
+		</div>
+		<footer id="the_footer">
+			Copyright MattEtherton 2015
+		</footer>
+	</div>
+</body>
+</html> 
+
+
+<?php
+echo 'Hello World';
+?>
